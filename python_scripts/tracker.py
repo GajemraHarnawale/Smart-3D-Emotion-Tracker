174be12a88331dbadcc0ab8322e55a9a097d3940
GajemraHarnawale <148611476+GajemraHarnawale@users.noreply.github.com>
2025-10-03 14:24:37 +0530

    Update tracker.py

--- python_scripts/tracker.py
+++ python_scripts/tracker.py
@@ -4,5 +4,5 @@ class Tracker:
     def __init__(self, max_age=30):
         self.tracker = DeepSort(max_age=max_age)
 
-    def update(self, detections, frame):
+    def update_tracks(self, detections, frame):
         return self.tracker.update_tracks(detections, frame=frame)
