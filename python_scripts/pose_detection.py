d0d6f3555e66c5995d4582fc7d1a5b313c9c9b0c
GajemraHarnawale <148611476+GajemraHarnawale@users.noreply.github.com>
2025-10-03 14:29:00 +0530

    Create pose_detection.py

---
+++ python_scripts/pose_detection.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+class PoseDetector:
+    def __init__(self, model_path="yolov8n-pose.pt"):
+        self.model = YOLO(model_path)
+
+    def detect(self, frame):
+        results = self.model(frame)[0]
+        return results
